d73fb0d93672907432d4d912f715993f318f6ceb
David Gettins <david@gettins.co.uk>
2020-12-01 10:49:31 +0000

    feat(init): add project files

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @typedef {import('ts-jest')} */
+/** @type {import('@jest/types').Config.InitialOptions} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  roots: ['<rootDir>/src/', '<rootDir>/test/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.[tj]sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testEnvironment: 'node',
+};
